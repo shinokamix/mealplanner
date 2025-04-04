3418bba5348a84cabc21b92f200face9c106b262
shinokamix <xxxxlovelovelove@gmail.com>
2025-04-04 14:57:43 +0300

    added telegram web api

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+'use client'
+
 import Image from "next/image";
 import Button from "../components/Button"
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+    // Подключаем Telegram Web App
+    const telegram = window.Telegram?.WebApp;
+    if (telegram) {
+      telegram.ready(); // Сообщаем Telegram, что приложение готово
+      console.log('Пользователь:', telegram.initDataUnsafe?.user); // Данные пользователя
+    }
+  }, []);
+   
   return (
       <div className="flex flex-col text-center h-[100vh] mt-0">
         <p className="text-8xl my-10">Meal Planner</p>
